315b1cc35346e1dfb2954eb1d5207a4bf84794dd
martin <martin.skara@hendur.eu>
2017-07-06 19:53:18 +0200

    0.1.0 release

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var glob = require("glob");
+var path = require("path");
+
+module.exports = function(from,to,strip,add) {
+    if (!add) {
+        add = "";
+    }
+    if (!strip) {
+        strip = "";
+    }
+    var stream = fs.createWriteStream(to);
+    glob.sync(from).forEach( function( file ) {
+        var helper = file.replace(/\.[^/.]+$/, "");
+        var helper2 = helper.replace(/-/g,"");
+        var name = helper2.match("[^/]+$");
+        file = file.replace(strip,"");
+        stream.write('const '+ name + ' = require("'+ add + file+'");\n');
+    });
+    stream.end();
+};
